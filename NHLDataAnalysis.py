ff953830bd9d73ece007760ded62a8993836b836
JeremyPK <jpkeeton@gmail.com>
2021-01-11 06:44:52 -0800

    dusting off Pandas notes
    NHL season is starting, woot.

---
+++ NHLDataAnalysis.py
@@ -0,0 +1,25 @@
+# Looking at NHL Data
+# Paste what I have so far
+
+import pandas as pd
+import xlrd
+# # add try accept here, test?
+# oh yah, so I can create a test function ensuring the that file is present?
+# can I create a test that will assert the file is of type. csv?
+# Create a variable for the file path
+#filename = 2019-2020.CLEANED.NHLStatsPersonalLapTopCopy.xlsx
+#file patch = C:/Users/jpkee/Documents/My Tableau Repository/Datasources
+# nbaStats = pd.read_csv("C:/Users/jpkee/Desktop/PythonProjects/NBA 2018-19 Season.csv")
+# nhlStats = pd.read_excel("C:/Users/jpkee/Documents/My Tableau Repository/Datasources/2019-2020.CLEANED.NHLStatsPersonalLapTopCopy.xlsx")
+# woops, need xlc
+# require_cols = [0, 3] 
+# nhlHead = nhlStats.head(10)
+print('Here\'s the top 10 rows: ' )
+# + nhlHead)
+# nhlInfo = nhlStats.info()
+# print(nhlHead)
+# print(nhlInfo)
+
+# required_df = pd.read_excel('C:/Users/jpkee/Documents/My Tableau Repository/Datasources/2019-2020.CLEANED.NHLStatsPersonalLapTopCopy.xlsx', usecols = require_cols) 
+  
+# print(required_df) 
